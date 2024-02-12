16e71b75085b13c01a6dd67e4a06f9636b4fd028
Alesia Paulava <sportgirl16031988@gmail.com>
2024-02-12 18:22:23 +0300

    fixed bag

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 // burger
 const addBurger = () => {
-  let btnBurger = document.querySelector('.header__burger');
-
-  // Смена состояния бургера
-  btnBurger.addEventListener('click', function () {
-
+  const toggleMenu = () => {
     document.querySelector('.section').classList.toggle('is-pointer-none');
     document.querySelector('.toggle-menu').classList.toggle('visible');
+    overlay.classList.toggle('open');
     document.body.classList.toggle('is-menu-open');
+  };
 
-  })
-}
+  const btnBurger = document.querySelector('.header__burger');
+  const overlay = document.querySelector('.overlay');
+
+  // Смена состояния бургера
+  btnBurger.addEventListener('click', toggleMenu);
+
+  // Скрывать меню при клике на overlay
+  overlay.addEventListener('click', toggleMenu);
+};
+
 addBurger();
 
 // choices
@@ -89,11 +95,23 @@ changeTextMobil();
 //строка поиска на мобилке
 const addSearchLine = () => {
   let searchLine = document.querySelector('.header__search-mobil');
-  // Смена состояния бургера
-  searchLine.addEventListener('click', function () {
-    document.querySelector('.search-mobil').classList.toggle('visible-mobil');
-  })
-}
+  let searchInput = document.querySelector('.search-mobil');
+
+  // Смена состояния строки поиска
+  searchLine.addEventListener('click', function (event) {
+    event.stopPropagation(); // Чтобы клик на строке поиска не срабатывал на document.body
+    searchInput.classList.toggle('visible-mobil');
+  });
+
+  // Обработчик события клика на body
+  document.body.addEventListener('click', function (event) {
+    // Если клик произошел не на строке поиска, не на инпуте и не на искомом элементе
+    if (!event.target.classList.contains('header__search-mobil') && !event.target.classList.contains('search-mobil') && event.target.tagName !== 'INPUT') {
+      searchInput.classList.remove('visible-mobil'); // Сворачиваем строку поиска
+    }
+  });
+};
+
 addSearchLine();
 
 //Модальное окно
